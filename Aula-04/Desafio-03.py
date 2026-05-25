4190f2be2c814f93116950380b4964fc11ea180a
Joao Victor Oliveira <joaovictorapcu@gmail.com>
2026-05-25 17:59:21 -0300

    Aula 04

--- Aula-04/Desafio-03.py
+++ Aula-04/Desafio-03.py
@@ -9,7 +9,7 @@ lado2 = float(input("Valor do segundo lado do triângulo: "))
 lado3 = float(input("Valor do terceiro lado do triângulo: "))
 
 if (lado1 + lado2 > lado3) and (lado1 + lado3 > lado2) and (lado2 +lado3 > lado1):
-    print("Seus valores podem formar um triângulo")
+    print("Seus valores formam um triângulo")
     
     if lado1 == lado2 == lado3:
         print("É um triângulo equilátero")
